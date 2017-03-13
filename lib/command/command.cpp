e62fac2249e3b4cd8e54970fe4da4ed60c942403
davidhubbard <david.c.hubbard@gmail.com>
2017-03-13 15:31:25 -0700

    add lib/command/fence.cpp

--- lib/command/command.cpp
+++ lib/command/command.cpp
@@ -145,26 +145,6 @@ int RenderPass::init(std::vector<PipelineCreateInfo> pcis) {
 	return 0;
 }
 
-int Semaphore::ctorError(language::Device& dev) {
-	VkSemaphoreCreateInfo VkInit(sci);
-	VkResult v = vkCreateSemaphore(dev.dev, &sci, nullptr, &vk);
-	if (v != VK_SUCCESS) {
-		fprintf(stderr, "vkCreateSemaphore returned %d (%s)\n", v, string_VkResult(v));
-		return 1;
-	}
-	return 0;
-}
-
-int Fence::ctorError(language::Device& dev) {
-	VkFenceCreateInfo VkInit(fci);
-	VkResult v = vkCreateFence(dev.dev, &fci, nullptr, &vk);
-	if (v != VK_SUCCESS) {
-		fprintf(stderr, "vkCreateFence returned %d (%s)\n", v, string_VkResult(v));
-		return 1;
-	}
-	return 0;
-}
-
 int CommandPool::ctorError(language::Device& dev, VkCommandPoolCreateFlags flags)
 {
 	auto qfam_i = dev.getQfamI(queueFamily);
@@ -204,25 +184,6 @@ int PresentSemaphore::ctorError() {
 
 	// Assume that any queue in this family is acceptable.
 	q = *(qfam.queues.end() - 1);
-	return 0;
-};
-
-int PresentSemaphore::present(uint32_t image_i) {
-	VkSemaphore semaphores[] = { vk };
-	VkSwapchainKHR swapChains[] = { dev.swapChain };
-
-	VkPresentInfoKHR VkInit(presentInfo);
-	presentInfo.waitSemaphoreCount = sizeof(semaphores)/sizeof(semaphores[0]);
-	presentInfo.pWaitSemaphores = semaphores;
-	presentInfo.swapchainCount = sizeof(swapChains)/sizeof(swapChains[0]);
-	presentInfo.pSwapchains = swapChains;
-	presentInfo.pImageIndices = &image_i;
-
-	VkResult v = vkQueuePresentKHR(q, &presentInfo);
-	if (v != VK_SUCCESS) {
-		fprintf(stderr, "vkQueuePresentKHR returned %d (%s)\n", v, string_VkResult(v));
-		return 1;
-	}
 	return 0;
 };
 
